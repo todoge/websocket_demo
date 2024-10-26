584f98317d2700c6bff44aab80392417ed9c01f3
Peh Jun Siang <austin9090@hotmail.com>
2024-10-26 18:35:07 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,4 +1,3 @@
-python manage.py migrate
-python manage.py collectstatic
-python -m daphne websocket_demo.asgi:application
-python -m gunicorn --workers 2 websocket_demo.asgi:application -k uvicorn_worker.UvicornWorker
+python3 manage.py migrate
+python3 manage.py collectstatic
+python3 -m gunicorn --workers 2 websocket_demo.asgi:application -k uvicorn_worker.UvicornWorker
