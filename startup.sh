848486c44fbc5148d9c8699adbd24c3a1b9e7db0
Peh Jun Siang <austin9090@hotmail.com>
2024-10-28 10:11:35 +0800

    update startup config

--- startup.sh
+++ startup.sh
@@ -1,2 +1,8 @@
 python manage.py migrate
 python manage.py collectstatic && gunicorn -k uvicorn.workers.UvicornWorker websocket_demo.asgi:application
+python -m gunicorn websocket_demo.wsgi:application --bind 0.0.0.0:8000 --reload & daphne -b 0.0.0.0 -p 8089
+websocket_demo.asgi:application &
+#
+#python3 -m gunicorn websocket_demo.wsgi:application --bind 127.0.0.1:8000 --reload & daphne -b 127.0.0.1 -p 8089
+#websocket_demo.asgi:application &
+#python -m gunicorn websocket_demo.wsgi:application --bind 0.0.0.0:8000 --reload & daphne -b 0.0.0.0 -p 8089 app.asgi:application &
