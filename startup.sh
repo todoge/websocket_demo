590e8072f97c3f2141004171d76e1e745e2cf4ba
junsiang.peh <junsiang.peh@okg.com>
2024-10-29 13:08:20 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,8 +1,8 @@
 python manage.py migrate
 #python manage.py collectstatic && gunicorn -k uvicorn.workers.UvicornWorker websocket_demo.asgi:application
 python manage.py collectstatic
-python -m gunicorn websocket_demo.wsgi:application --bind 0.0.0.0:8000
 python -m daphne -b 0.0.0.0 -p 8089 websocket_demo.asgi:application
+python -m gunicorn websocket_demo.wsgi:application --bind 0.0.0.0:8000
 #python3 -m gunicorn websocket_demo.wsgi:application --bind 127.0.0.1:8000 --reload & daphne -b 127.0.0.1 -p 8089
 #websocket_demo.asgi:application &
 #python -m gunicorn websocket_demo.wsgi:application --bind 0.0.0.0:8000 --reload & daphne -b 0.0.0.0 -p 8089 app.asgi:application &
