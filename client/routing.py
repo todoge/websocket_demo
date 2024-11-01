8f83adb7cf1451cde75bbda400df70edeacee2ad
Peh Jun Siang <austin9090@hotmail.com>
2024-11-02 02:53:49 +0800

    Update routing.py

--- client/routing.py
+++ client/routing.py
@@ -3,7 +3,12 @@ from django.urls import re_path
 from client import consumer
 from django.urls import path
 
+# websocket_urlpatterns = [
+#     re_path(r'^ws/channel1/*.$', consumer.ConsumerChannel1.as_asgi()),  # Define your WebSocket endpoint
+#     re_path(r'^ws/channel2/.*$', consumer.ConsumerChannel2.as_asgi()),
+# ]
+
 websocket_urlpatterns = [
-    re_path(r'^ws/channel1/*.$', consumer.ConsumerChannel1.as_asgi()),  # Define your WebSocket endpoint
-    re_path(r'^ws/channel2/.*$', consumer.ConsumerChannel2.as_asgi()),
+    path('ws/channel1/', consumer.ConsumerChannel1.as_asgi()),  # Matches exactly /ws/channel1/
+    path('ws/channel2/', consumer.ConsumerChannel2.as_asgi()),  # Matches exactly /ws/channel2/
 ]
