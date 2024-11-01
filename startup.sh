d1fa2e13954c06b729b23cc8c5632a8059dfc103
Peh Jun Siang <austin9090@hotmail.com>
2024-11-02 02:10:36 +0800

    Update startup.sh

--- startup.sh
+++ startup.sh
@@ -1,9 +1,9 @@
 python manage.py migrate
 python manage.py collectstatic
 # Start Daphne for handling ASGI/WebSocket connections
-python -m daphne -b 0.0.0.0 -p 8089 websocket_demo.asgi:application &
+python -m daphne -b 0.0.0.0 -p 8000 websocket_demo.asgi:application
 # Start Gunicorn for handling WSGI/HTTP requests
-python -m gunicorn websocket_demo.wsgi:application --bind 0.0.0.0:8000
+# python -m gunicorn websocket_demo.wsgi:application --bind 0.0.0.0:8000
 #python3 -m gunicorn websocket_demo.wsgi:application --bind 127.0.0.1:8000 --reload & daphne -b 127.0.0.1 -p 8089
 #websocket_demo.asgi:application &
 #python -m gunicorn websocket_demo.wsgi:application --bind 0.0.0.0:8000 --reload & daphne -b 0.0.0.0 -p 8089 app.asgi:application &
